6948ab287fe472f6a872e99aabdad4f639f0eb74
Nitesh Singla <snitesh91@gmail.com>
2017-05-28 18:48:16 +0530

    logging

--- routes/restricted.js
+++ routes/restricted.js
@@ -50,28 +50,33 @@ let mailOptions = {
 
 	//if client is available sent it to him else sent to his email
 router.post('/sendToClient', function(req, res, next) {
+	console.log('request received');
 	var user = req.body.user;
 	var channel = "/" + user.guestId;
 
 	var map = req.app.locals.subscriptionMap;
 	if(map.has(channel)){
-			
+			console.log('user already subscriber');
 			var pub = fayeClient.publish(channel,{
 			msg: req.body.message
 		});
+			console.log('message send to server on faye');
 		pub.then(function() {
 	 	 	res.sendStatus(200);
 		}, function(error) {
 	  		res.sendStatus(400);
 		});
 	}else{
+		console.log('user not subscribed sending mail');
 		mailOptions.to = user.emailId;
 		mailOptions.text = req.body.message;
 		mailOptions.html = '<b>'+req.body.message+'</b>';
 		transporter.sendMail(mailOptions, function(error, info) {
+			console.log('mail sending');
 		    if (error) {
 		       res.sendStatus(400);
 		    }
+		    console.log('mail send');
 		    res.sendStatus(200);
 		});
 	}
